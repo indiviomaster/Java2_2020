8e79eb85fd050694c7a0981daa75a211e6303408
indivioM <indivio@yandex.ru>
2020-03-25 22:18:23 +0300

    wall3

--- src/lesson1/Trails.java
+++ src/lesson1/Trails.java
@@ -4,7 +4,7 @@ public class Trails {
     public static void main(String[] args) {
 
         Player [] players = {new Cat(4, 4),new Robot(8,8),new Human(2,10)};
-        Barrier [] tracks = {new Track(2), new Wall(2), new Wall(5),new Track(4), new Wall(2),new Track(8)};
+        Barrier [] tracks = {new Track(2), new Wall(3), new Wall(5),new Track(4), new Wall(2),new Track(8)};
 
         for(int j=0; j<players.length; j++) {
             System.out.println(players[j].toString());
