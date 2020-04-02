462c5f5b30f7775dc27378077c5c79689b7e1864
indivioM <indivio@yandex.ru>
2020-04-02 19:33:25 +0300

    Change Set to Map

--- src/lesson3/TenWords.java
+++ src/lesson3/TenWords.java
@@ -1,9 +1,6 @@
 package lesson3;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class TenWords {
     public static void main(String[] args) {
@@ -11,21 +8,18 @@ public class TenWords {
         checkWords(stringArray);
     }
     private static void checkWords(String[] arr) {
-        List<String> listWords = new ArrayList<>();
-        Set <String> setWords = new HashSet<>();
-
+        Map<String, Integer> mapWords = new HashMap<>();
+        int k=0;
         for (int i=0; i<arr.length;i++){
-            listWords.add(arr[i]);
-            setWords.add(arr[i]);
-        }
 
-        for (String o:setWords){
-            int count =0;
-            for(String l:listWords)
-                if (o.equals(l)){
-                count ++;}
-            System.out.println(o + " -> " +count+"шт");
-        }
+            if (mapWords.get(arr[i])==null) {
+                k = 1;
 
-    }
+            }else{
+                k = mapWords.get(arr[i]) +1;
+            }
+            mapWords.put(arr[i], k);
+        }
+        System.out.println(mapWords);
+        }
 }
